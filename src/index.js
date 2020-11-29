fe4956852cebf9b42241224073120bd2ba82bad8
Jorge Gonzalez <jglez.cs@gmail.com>
2020-11-28 22:32:13 -0600

    Created a booklist component with nested components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+function Book() {
+  return(
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
+
+function Image() {
+  return(
+    <img src='https://images-na.ssl-images-amazon.com/images/I/410hiaPGyCL._SX348_BO1,204,203,200_.jpg'/>
+  );
+}
+
+function Title() {
+  return (
+    <h1>Cracking the Coding Interview</h1>
+  )
+}
+
+function Author() {
+  return (
+    <h2>Gayle Mcdowell</h2>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BookList />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
